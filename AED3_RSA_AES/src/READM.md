15944949264fa1a0de30c1830a6c116bac6c1dd0
Lucas Morais <lucas.morais@visagio.com>
2022-10-23 15:06:00 -0300

    gen correcly hash

--- AED3_RSA_AES/src/READM.md
+++
@@ -1,1 +0,0 @@
-# RSA and AES
--- AED3_RSA_AES/src/main.cpp
+++ AED3_RSA_AES/src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <iomanip>
 
-int main(int* argc, char** argv)
+
+int main(int argc, char** argv)
 {
-	
+	std::string file_name;
+	if (argc < 2) file_name = "decoded-20221023004659.bin";
+	else file_name = argv[1];
+	std::ifstream solution(file_name, std::ios::binary);
+	std::vector<unsigned char> binary_content(std::istreambuf_iterator<char>(solution), {});
+
+	for (int i = 0; i < binary_content.size(); i++)
+	{
+		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)binary_content[i];
+	}
+	std::cin.get();
 	return 0;
 }
