bcf238865421fa1ec19bc0f28f8d9c95a2a821b7
Lucas Morais <lucas.morais@visagio.com>
2022-10-25 14:52:08 -0300

    final commit

--- AED3_RSA_AES/src/main.cpp
+++ AED3_RSA_AES/src/main.cpp
@@ -19,5 +19,12 @@ int main(int argc, char** argv)
 		std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)binary_content[i];
 	}
 	std::cin.get();
+
+	// hashcat -m(hash type - SHA2-256) 1400 -a(attack mode - Brute Force) 3 -d(device - GPU) 2 
+	// 6d00863e4fd4be846d2f3817635e9b0a8a08c4f5b1243a6a8d215dd2f97bbbd3 yo?l?l?l?l?l?l?l?l -i(increment) 
+	// -o(output file) decripted_hash.txt -O(optmized) 
+
+	// hashcat -m 1400 -a 3 -d 2 6d00863e4fd4be846d2f3817635e9b0a8a08c4f5b1243a6a8d215dd2f97bbbd3 yo?l?l?l?l?l?l?l?l?l?l -o decripted_hash.txt -O
+
 	return 0;
 }
